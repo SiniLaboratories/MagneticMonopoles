f10c001fac319df9f0b5bcdf666892a9982ce9b1
Sini <sini.laboratories@gmail.com>
2026-01-13 10:09:44 +0100

    Fix structs and index functions

--- src/Structs.rs
+++ src/Structs.rs
@@ -1,32 +1,33 @@
 //Struct of a single physical point in space
+#[derive(Default,Serialize, Deserialize, Clone)]
 pub struct Point {
-    pub rhoem: f64 = 0.0,
-    pub v_em: [f64; 3] = [0.0, 0.0, 0.0];
-    pub rhoep: f64 = 0.0,
-    pub v_ep: [f64; 3] = [0.0, 0.0, 0.0];
-    pub rhomm: f64 = 0.0,
-    pub v_mm: [f64; 3] = [0.0, 0.0, 0.0];
-    pub rhomp: f64 = 0.0,
-    pub v_mp: [f64; 3] = [0.0, 0.0, 0.0];
+    pub rhoem: f64,
+    pub v_em: [f64; 3],
+    pub rhoep: f64,
+    pub v_ep: [f64; 3],
+    pub rhomm: f64,
+    pub v_mm: [f64; 3],
+    pub rhomp: f64,
+    pub v_mp: [f64; 3],
 
-    pub E: [f64; 3] = [0.0, 0.0, 0.0];
-    pub B: [f64; 3] = [0.0, 0.0, 0.0];
+    pub E: [f64; 3],
+    pub B: [f64; 3]
 }
 
 //Struct of the time derivative of a physical point in space
 
 pub struct DtPoint {
-    pub rhoem: f64 = 0.0,
-    pub v_em: [f64; 3] = [0.0, 0.0, 0.0];
-    pub rhoep: f64 = 0.0,
-    pub v_ep: [f64; 3] = [0.0, 0.0, 0.0];
-    pub rhomm: f64 = 0.0,
-    pub v_mm: [f64; 3] = [0.0, 0.0, 0.0];
-    pub rhomp: f64 = 0.0,
-    pub v_mp: [f64; 3] = [0.0, 0.0, 0.0];
+    pub rhoem: f64,
+    pub v_em: [f64; 3],
+    pub rhoep: f64,
+    pub v_ep: [f64; 3],
+    pub rhomm: f64,
+    pub v_mm: [f64; 3],
+    pub rhomp: f64,
+    pub v_mp: [f64; 3].
 
-    pub E: [f64; 3] = [0.0, 0.0, 0.0];
-    pub B: [f64; 3] = [0.0, 0.0, 0.0];
+    pub E: [f64; 3],
+    pub B: [f64; 3]
 }
 
 //Struct of configuration file --> Do I really need it??
--- src/index_conversion.rs
+++ src/index_conversion.rs
@@ -1,6 +1,6 @@
 
 //Function that converts index i to ix iy iz
-pub fn index_to_3d(i:usize, nx:usize, ny:usize, nz:usize) -> (usize, usize, usize)
+pub fn index_to_3d(i:usize, nx:usize, ny:usize, _nz:usize) -> (usize, usize, usize)
 {
     let iz = i / (nx*ny);
     let iy = (i - iz*nx*ny) / nx;
@@ -13,7 +13,10 @@ pub fn index_to_3d(i:usize, nx:usize, ny:usize, nz:usize) -> (usize, usize, usiz
 //Function that converts ix iy iz to index i
 pub fn index_from_3d(ix:usize, iy:usize, iz:usize, nx:usize, ny:usize, nz:usize) -> usize
 {
-    let i = iz*nx*ny + iy*nx + ix;
+    let ix2 = ix % nx;
+    let iy2 = iy % ny;  
+    let iz2 = iz % nz;
+    let i = iz2*nx*ny + iy2*nx + ix2;
 
     i
 }
